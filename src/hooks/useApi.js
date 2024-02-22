c2212e5eee8989b26d8ccdde483b698f37a6b401
David Rimar <rimardavid@gmail.com>
2024-02-22 22:58:46 +0100

    adding new suggestion

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -6,12 +6,19 @@ export const AXIOS_METHOD = {
     'POST': 'POST'
 };
 
+let authToken = false;
+
+export function setApiToken(newToken) {
+    authToken = newToken;
+};
+
 export function makeApiCall(method, url, onSuccess, onFailure = false, data = {}) {
     let axiosCall = method === AXIOS_METHOD.POST ? axios.post : axios.get;
     axios({
         method,
         url: `${BASE_URL}${url}`,
-        data
+        data,
+        headers: authToken !== false ? {'Authorization': `Bearer ${authToken}`} : {}
     }).then(response => {
         onSuccess(response.data);
     }).catch(error => {
--- src/screens/suggestions/add/AddScreen.js
+++ src/screens/suggestions/add/AddScreen.js
@@ -2,13 +2,27 @@ import { Grid, Typography } from "@mui/material";
 import { Form, Formik, Field } from "formik";
 import { TextField } from "formik-mui";
 import SubmitButton from "../../../components/SubmitButton";
+import { useNavigate } from "react-router-dom";
+import { makeApiCall, AXIOS_METHOD } from "../../../hooks/useApi";
 
 
 function AddScreen() {
+    const navigate = useNavigate();
+
     return (<>
             <Typography variant={"h4"}>Add new suggestion</Typography>
             <br/>
-            <Formik initialValues={{}}>
+            <Formik initialValues={{}} onSubmit={(values, actions) => {
+                actions.setSubmitting(true);
+                makeApiCall(AXIOS_METHOD.POST, '/add_suggestion', (_unusedData) => {
+                    // if success, navigate to suggestion details page
+                    actions.setSubmitting(false);
+                    navigate("/myprofile");
+                }, (apiError) => {
+                    actions.setFieldError('title', apiError);
+                    actions.setSubmitting(false);
+                }, values); 
+            }}>
                 <Form>
                     <Grid container spacing={2}>
                         <Grid item xs={12}>
